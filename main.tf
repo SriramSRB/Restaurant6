fcbe80f636dbb59f233b243dbc51d8deee89cdb8
SriramSRB <ssrirambabu27@gmail.com>
2026-04-23 11:54:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,32 +39,32 @@ resource "aws_security_group" "restaurant_sg" {
         from_port   = 22
         to_port     = 22
         protocol    = "tcp"
-        cidr_blocks = "0.0.0.0/0"
+        cidr_blocks = ["0.0.0.0/0"]
     }
     ingress {
         from_port   = 80
         to_port     = 80
         protocol    = "tcp"
-        cidr_blocks = "0.0.0.0/0"
+        cidr_blocks = ["0.0.0.0/0"]
     }
     ingress {
         from_port   = 8080
         to_port     = 8080
         protocol    = "tcp"
-        cidr_blocks = "0.0.0.0/0"
+        cidr_blocks = ["0.0.0.0/0"]
     }
     ingress {
         from_port   = 30080
         to_port     = 30080
         protocol    = "tcp"
-        cidr_blocks = 30080
+        cidr_blocks = ["0.0.0.0/0"]
     }
 
     egress {
         from_port   = 0
         to_port     = 0
         protocol    = "-1"
-        cidr_blocks = "0.0.0.0/0"
+        cidr_blocks = ["0.0.0.0/0"]
     }
 }
 
